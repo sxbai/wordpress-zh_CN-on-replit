d6a239f9dfd96ece1c06a450a6f5cd44070e2a74
舒夏 <55978160+sxbai@users.noreply.github.com>
2023-02-20 22:16:32 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 git clone https://github.com/sxbai/wordpress-zh_CN-on-replit.git
-cp -r replit.nix . && mv wordpress-zh_CN-on-replit/* .
+cp -r wordpress-zh_CN-on-replit/replit.nix replit.nix && mv wordpress-zh_CN-on-replit/* .
 rm -rf wordpress-zh_CN-on-replit/ && rm db.php
 bash install.sh
